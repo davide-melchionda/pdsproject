a33cb3b102141fa7fe822ce12ed865ade94ca80e
Francesco Politano <francesco.politano91@hotmail.com>
2017-10-17 19:45:11 +0200

    Client and Server interfaces defined

---
+++ FileShareConsole/FileShareConsole/PacketServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTransmission
+{
+    class PacketServer : ExecutableThread
+    {
+
+        public delegate void PacketReceivedDel(Packet p);
+        public event PacketReceivedDel OnPacketReceived;
+
+        public delegate void TrasmissionEnddDel(Packet p);
+        public event TrasmissionEnddDel OnTrasmissionEnd;
+
+        public PacketServer() { }
+
+        protected override void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
+
