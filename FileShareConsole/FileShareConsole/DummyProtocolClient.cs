ad32639a179d98d8e0a4994a14a110f411e2f45d
davide-melchionda <davide.melchionda.it@gmail.com>
2017-10-20 12:04:48 +0200

    JobScheduler, ProtocolEndpoint and JobZipMemoryModule
    WHAT'S NEW
    + JobScheduler implemented. JobScheduler is no more an executable thread, but it's only a class which can be instantiated and passed to the TaskCreator (not implemented yet) and that offers a method scheduleJob(Job j) that allows to start the job computation on a dedicated thread.
    Note: in this way, the ToDoJobQueue does not have sense anymore.
    + JobZipMemoryModule and inner class JobFileShare have replaced ZIPMemoryModule and FileTransferIterator respectively. They are now aware of the job existence and are oriented to preparing the file linked to the job to be sent on the network (it will be zipped) and to manage this sending (while sending bytes, the job completion level is coherently incremented by the JobFileIterator).
    + ProtocolEndpoing, ProtocolEndpointClient and ProtocolEndpointServer have been implemented in order to hide the complexity of a client and of a server that implement a specific protocol on a given connected socket.
    
    WHAT'S MISSING
    - All the server side management. Actually a DummyProtocolClient is an implementation of ClientProtocolEndpoint which print all byte received on a file.

---
+++ FileShareConsole/FileShareConsole/DummyProtocolClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Sockets;
+using NetworkTransmission;
+using Protocol;
+using static StorageModule;
+using System.Text;
+using static Protocol.ProtocolEndpoint;
+using FileShareConsole;
+using System.IO;
+
+namespace FileTransfer {
+
+    public class DummyClient : ClientProtocolEndpoint {
+
+        private FileIterator iterator;
+        private Task task;
+
+        public DummyClient(Socket socket, FileIterator iterator, Task task) : base(socket) {
+            this.iterator = iterator;
+            this.task = task;
+        }
+
+        public override HandshakeResult handshake() {
+            throw new NotImplementedException();
+        }
+
+        public override TransferResult transfer() {
+            byte[] buf = new Byte[JobZipStorageModule.READ_BLOCK_SIZE+1];
+            while (iterator.hasNext()) {
+                int read = iterator.next(buf);
+                FileStream f = File.Create(@"C:\Users\vm-dm-win\Desktop\exampleFile.txt");
+                f.Write(buf, 0, read);
+                f.Close();
+            }
+
+            System.Threading.Thread.Sleep(10000);
+
+            return new DummyTransferResult();
+        }
+    }
+
+    public class DummyServer : ClientProtocolEndpoint {
+        public DummyServer(Socket socket) : base(socket) {
+        }
+
+        public override HandshakeResult handshake() {
+            throw new NotImplementedException();
+        }
+
+        public override TransferResult transfer() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DummyHandshakeResult : HandshakeResult {
+
+            public enum HResult {
+                ACCEPTED,
+                REFUSED,
+                AUTHN_ERROR
+            };
+
+            private HResult result;
+            public HResult Result {
+                get {
+                    return result;
+                }
+            }
+        }
+
+       public class DummyTransferResult : TransferResult {
+
+        }
+    }
--- FileShareConsole/FileShareConsole/Program.cs
+++ FileShareConsole/FileShareConsole/Program.cs
@@ -1,9 +1,13 @@
-﻿using HelloProtocol;
+﻿using FileTransfer;
+using HelloProtocol;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static StorageModule;
 
 namespace FileShareConsole
 {
@@ -11,8 +15,37 @@ namespace FileShareConsole
     {
         static void Main(string[] args) {
 
-            new HelloThread().run();
+            //new HelloThread().run();
+
+            //new JobScheduler().run();
+
+            JobZipStorageModule storage = new JobZipStorageModule();
             
+            string s = @"C:\Users\vm-dm-win\Desktop\tmp.txt";
+            string to = @"C:\Users\vm-dm-win\Desktop\tmpzipped"+DateTime.Now.Millisecond+@".zip";
+
+            //ZipFile.CreateFromDirectory(s, to);
+
+            //ZipArchive newFile = ZipFile.Open(to, ZipArchiveMode.Create);
+            //string p = Path.GetDirectoryName(s);
+            //newFile.CreateEntryFromFile(s, Path.GetFileName(s), CompressionLevel.NoCompression);
+            //newFile.Dispose();
+
+            //FileIterator i = storage.prepareFile(s, DateTime.Now);
+            //FileIterator i = storage.prepareFile(@"C:\Users\vm-dm-win\Desktop\tmp", new DateTime());
+            //System.Threading.Thread.Sleep(20000);
+
+            //new JobScheduler().run();
+
+
+
+            PeersList.Instance.put(new Peer("you", "you", "127.0.0.1"));
+
+            JobScheduler scheduler = new JobScheduler();
+            scheduler.scheduleJob(new Job(new FileTransfer.Task("me","you",s),s));
+
+            //i.close();
+
         }
     }
 }
